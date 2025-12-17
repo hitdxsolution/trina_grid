826fdbf695d304f38ea0a6e81b679419690f1444
byunsh96 <152119793+byunsh96@users.noreply.github.com>
2025-12-17 09:50:49 +0900

    [add] TrinaCell rawValue getter 추가

--- lib/src/model/trina_cell.dart
+++ lib/src/model/trina_cell.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trina_grid/trina_grid.dart';
 
-typedef TrinaCellRenderer = Widget Function(
-    TrinaCellRendererContext rendererContext);
+typedef TrinaCellRenderer = Widget Function(TrinaCellRendererContext rendererContext);
 
 class TrinaCellRendererContext {
   final TrinaColumn column;
@@ -106,6 +105,11 @@ class TrinaCell {
       _applyFormatOnInit();
     }
 
+    return _value;
+  }
+
+  // no format applied value
+  dynamic get rawValue {
     return _value;
   }
 
